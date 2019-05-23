405228055d6d8872b829cd4c83593ef161c75594
infosalim <salimh5757@gmail.com>
2019-05-24 03:44:33 +0600

    created the header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './resources/styles.css';
+import Header from './components/header_footer/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Md Salim Hossain</h1>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+      </div>
+    );
+  }
 }
 
 export default App;
