c753e72717b8cd5a1620097665f00ea616226e76
infosalim <salimh5757@gmail.com>
2019-05-25 02:00:49 +0600

    created the slider with react-slick

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './resources/styles.css';
+
 import Header from './components/header_footer/Header';
+import Featured from './components/Featured/Index';
 
 class App extends Component {
 
@@ -8,6 +10,7 @@ class App extends Component {
     return (
       <div className="App" style={{height: "30000px", background: "lightBlue"}}>
         <Header/>
+        <Featured />
       </div>
     );
   }
