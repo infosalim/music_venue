9a20ca4e0e4e725d0968b6b220306b5ca02dbfe6
infosalim <salimh5757@gmail.com>
2019-05-24 16:53:48 +0600

    created the side drawer

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import MenuIcon from '@material-ui/core/Menu';
-// import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+
+import SideDrawer from './SideDrawer';
+
 
 class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
+            drawerOpen: false
+        }
+    }
+
+    toggleDrawer = value => {
+        this.setState({
+            drawerOpen: value
+        });
+    }
     render() {
         return (
             <AppBar
@@ -16,12 +31,23 @@ class Header extends Component {
                     padding: '10px 0'
                 }}
             >
-            <Toolbar>
-                <div className="header_logo">
-                    <div className="font_righteous header_logo_venue">Music Venue</div>
-                    <div className="header_logo_title">Musical Events</div>
-                </div>
-            </Toolbar>
+                <Toolbar>
+                    <div className="header_logo">
+                        <div className="font_righteous header_logo_venue">Music Venue</div>
+                        <div className="header_logo_title">Musical Events</div>
+                    </div>
+                    <IconButton
+                        area-label="Menu"
+                        color="inherit"
+                        onClick={() => this.toggleDrawer(true)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <SideDrawer
+                        open={this.state.drawerOpen}
+                        close={value => this.toggleDrawer(value)}
+                    />
+                </Toolbar>
             </AppBar>
         );
     }
